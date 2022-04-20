38a75b844544086cebb1ec669bf4ef00ed2265ae
Matthew Parker <mattparker124@gmail.com>
2022-04-20 17:10:49 -0500

    added employee methods

--- ___tests___/Employee.test.js
+++ ___tests___/Employee.test.js
@@ -6,4 +6,24 @@ test('creates an employee object', () => {
     expect(employee.name).toBe('Jeff');
     expect(employee.id).toBe('1');
     expect(employee.email).toBe('jeff@work.com');
+});
+
+test('getName() gets the name of an employee', () => {
+    const employee = new Employee('Jeff', '1', 'jeff@work.com');
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test('getId() gets the id of an employee', () => {
+    const employee = new Employee('Jeff', '1', 'jeff@work.com');
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id));
+});
+
+test('getEmail() gets the email of an employee', () => {
+    const employee = new Employee('Jeff', '1', 'jeff@work.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
+
+test('getRole() returns Employee', () => {
+    const employee = new Employee('Jeff', '1', 'jeff@work.com');
+    expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
 })
